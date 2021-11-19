cda65e3745ab66aa8bc6bf91e1c37d8e2b5a7b16
lapshina.vk <lapshina.vk@gmail.com>
2021-11-19 17:20:35 +0000

    It's done

---
+++ repeat_str06.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILE=$1
+TEXT=$2
+REP=$3
+
+if [[ -e $FILE ]]
+then echo "Error: Such file- ${FILE} exists"
+else
+	touch $FILE
+	while [[ $(cat $FILE| wc -l) -lt $REP ]]; do
+		echo "$TEXT" >> $FILE
+	done
+fi
