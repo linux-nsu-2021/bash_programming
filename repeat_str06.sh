6374db14f84eb00b2e867b8af829abeb38b500ad
lapshina.vk <lapshina.vk@gmail.com>
2021-11-20 13:59:44 +0000

    It done

--- repeat_str06.sh
+++ repeat_str06.sh
@@ -5,7 +5,7 @@ TEXT=$2
 REP=$3
 
 if [[ -e $FILE ]]
-then echo "Error: Such file- ${FILE} exists"
+then echo "Error: Such file '${FILE}' exists"
 else
 	touch $FILE
 	while [[ $(cat $FILE| wc -l) -lt $REP ]]; do
